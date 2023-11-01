a1b5eb9a458d54b660661c4e828d0f2267e093fc
dh365x <dh365x@gmail.com>
2023-11-01 15:46:49 +0900

    Form, validation

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -2,17 +2,22 @@ import { useState } from "react";
 
 function ToDoList() {
 	const [toDo, setToDo] = useState("");
+	const [toDoError, setToDoError] = useState("");
 
 	const onChange = (event: React.FormEvent<HTMLInputElement>) => {
 		const {
 			currentTarget: { value },
 		} = event;
+		setToDoError("");
 		setToDo(value);
 	};
 
 	const onSubmit = (event: React.FocusEvent<HTMLFormElement>) => {
 		event.preventDefault();
-		console.log(toDo);
+		if (toDo.length < 2) {
+			return setToDoError("it should be longer");
+		}
+		console.log("submit");
 		setToDo("");
 	};
 
@@ -20,6 +25,7 @@ function ToDoList() {
 		<>
 			<form onSubmit={onSubmit}>
 				<input value={toDo} onChange={onChange} placeholder="Write a to do" />
+				{toDoError !== "" ? toDoError : null}
 				<button>Add</button>
 			</form>
 		</>
