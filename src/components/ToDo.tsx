d18237e0fb77e184ebbf42a247ab967ed5e7e89a
dh365x <dh365x@gmail.com>
2023-11-01 18:28:49 +0900

    Categories, mouse event & button tag

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,31 @@
 import { IToDo } from "../recoil/atoms";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category }: IToDo) {
+	const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+		const {
+			currentTarget: { name },
+		} = event;
+		console.log(name);
+	};
+
 	return (
 		<li>
 			<span>{text}</span>
-			<button>Doing</button>
-			<button>To Do</button>
-			<button>Done</button>
+			{category !== "DOING" && (
+				<button name="DOING" onClick={onClick}>
+					Doing
+				</button>
+			)}
+			{category !== "TO_DO" && (
+				<button name="TO_DO" onClick={onClick}>
+					To Do
+				</button>
+			)}
+			{category !== "DONE" && (
+				<button name="DONE" onClick={onClick}>
+					Done
+				</button>
+			)}
 		</li>
 	);
 }
